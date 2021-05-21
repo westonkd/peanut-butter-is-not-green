ee38da0aeb40e7be62ae8ac7702d12eca078ab14
Weston Dransfield <wdransfield@instructure.com>
2021-05-21 09:38:03 -0600

    2. Use Contexts
    Contexts are a powerful method to make your tests
    clear and well organized (they keep tests easy to read).
    
    When describing a context, start its description
    with 'when', 'with' or 'without'.

--- spec/controllers/peanut_butter_controller_spec.rb
+++ spec/controllers/peanut_butter_controller_spec.rb
@@ -4,138 +4,171 @@ require_relative '../spec_helper'
 
 describe PeanutButterController do
   describe '#show' do
-    it 'shows the peanut butter with status 200 when it exists' do
-      user = User.create!(
-        given_names: 'Weston K.',
-        surnames: 'Dransfield'
-      )
+    context 'when the peanut butter exists' do
+      it 'shows the peanut butter with status 200' do
+        user = User.create!(
+          given_names: 'Weston K.',
+          surnames: 'Dransfield'
+        )
 
-      pb = PeanutButter.create!(
-        brand: 'Jif',
-        price_per_ounce: 13.8,
-        average_rating: 3.5,
-        color: 'not green'
-      )
+        pb = PeanutButter.create!(
+          brand: 'Jif',
+          price_per_ounce: 13.8,
+          average_rating: 3.5,
+          color: 'not green'
+        )
 
-      response = PeanutButterController.new(
-        user: user, params: { id: pb.id }
-      ).show
+        response = PeanutButterController.new(
+          user: user, params: { id: pb.id }
+        ).show
 
-      expect(response).to be_ok
-      expect(response.body[:brand]).to eq 'Jif'
-      expect(response.body[:price_per_ounce]).to eq 13.8
-      expect(response.body[:average_rating]).to eq 3.5
-      expect(response.body[:color]).to eq 'not green'
+        expect(response).to be_ok
+        expect(response.body[:brand]).to eq 'Jif'
+        expect(response.body[:price_per_ounce]).to eq 13.8
+        expect(response.body[:average_rating]).to eq 3.5
+        expect(response.body[:color]).to eq 'not green'
+      end
     end
 
-    it 'renders 401 when no user session exists' do
-      pb = PeanutButter.create!(
-        brand: 'Jif',
-        price_per_ounce: 13.8,
-        average_rating: 3.5,
-        color: 'not green'
-      )
+    context 'when no user session exists' do
+      it 'renders 401' do
+        pb = PeanutButter.create!(
+          brand: 'Jif',
+          price_per_ounce: 13.8,
+          average_rating: 3.5,
+          color: 'not green'
+        )
 
-      response = PeanutButterController.new(
-        user: nil, params: { id: pb.id }
-      ).show
+        response = PeanutButterController.new(
+          user: nil, params: { id: pb.id }
+        ).show
 
-      expect(response).to be_unauthorized
-      expect(response.body[:message]).to eq 'Requires an active user session'
+        expect(response).to be_unauthorized
+        expect(response.body[:message]).to eq 'Requires an active user session'
+      end
     end
 
-    it 'renders 404 and an error message if the PB does not exist' do
-      user = User.create!(
-        given_names: 'Weston K.',
-        surnames: 'Dransfield'
-      )
+    context 'when the peanut butter does not exist' do
+      it 'renders 404' do
+        user = User.create!(
+          given_names: 'Weston K.',
+          surnames: 'Dransfield'
+        )
 
-      response = PeanutButterController.new(user: user, params: { id: 999 }).show
-      expect(response).to be_not_found
-      expect(response.body[:message]).to eq 'A record with the given ID does not exist'
+        response = PeanutButterController.new(user: user, params: { id: 999 }).show
+        expect(response).to be_not_found
+        expect(response.body[:message]).to eq 'A record with the given ID does not exist'
+      end
     end
   end
 
   describe '#update' do
-    it 'updates the peanut butter' do
-      pb = PeanutButter.create!(
-        brand: 'Jif',
-        price_per_ounce: 13.8,
-        average_rating: 3.5,
-        color: 'not green'
-      )
+    context 'when the peanut butter exists' do
+      it 'updates the peanut butter' do
+        pb = PeanutButter.create!(
+          brand: 'Jif',
+          price_per_ounce: 13.8,
+          average_rating: 3.5,
+          color: 'not green'
+        )
 
-      user = User.create!(
-        given_names: 'Weston K.',
-        surnames: 'Dransfield',
-        is_admin: true
-      )
+        user = User.create!(
+          given_names: 'Weston K.',
+          surnames: 'Dransfield',
+          is_admin: true
+        )
 
-      PeanutButterController.new(
-        user: user,
-        params: {
-          id: pb.id,
-          peanut_butter: {
-            average_rating: 5.0
+        PeanutButterController.new(
+          user: user,
+          params: {
+            id: pb.id,
+            peanut_butter: {
+              average_rating: 5.0
+            }
           }
-        }
-      ).update
+        ).update
 
-      expect(PeanutButter.find(pb.id).average_rating).to eq 5
+        expect(PeanutButter.find(pb.id).average_rating).to eq 5
+      end
+
+      it 'responds with the updated peanut butter' do
+        pb = PeanutButter.create!(
+          brand: 'Jif',
+          price_per_ounce: 13.8,
+          average_rating: 3.5,
+          color: 'not green'
+        )
+
+        user = User.create!(
+          given_names: 'Weston K.',
+          surnames: 'Dransfield',
+          is_admin: true
+        )
+
+        response = PeanutButterController.new(
+          user: user,
+          params: {
+            id: pb.id,
+            peanut_butter: {
+              average_rating: 5.0
+            }
+          }
+        ).update
+
+        expect(response).to be_ok
+        expect(response.body[:average_rating]).to eq 5.0
+      end
     end
 
-    it 'responds with the updated peanut butter' do
-      pb = PeanutButter.create!(
-        brand: 'Jif',
-        price_per_ounce: 13.8,
-        average_rating: 3.5,
-        color: 'not green'
-      )
+    context 'when the current user is not an admin' do
+      it 'does not allow updating PB' do
+        pb = PeanutButter.create!(
+          brand: 'Jif',
+          price_per_ounce: 13.8,
+          average_rating: 3.5,
+          color: 'not green'
+        )
 
-      user = User.create!(
-        given_names: 'Weston K.',
-        surnames: 'Dransfield',
-        is_admin: true
-      )
+        user = User.create!(
+          given_names: 'Weston K.',
+          surnames: 'Dransfield',
+        )
 
-      response = PeanutButterController.new(
-        user: user,
-        params: {
-          id: pb.id,
-          peanut_butter: {
-            average_rating: 5.0
+        response = PeanutButterController.new(
+          user: user,
+          params: {
+            id: pb.id,
+            peanut_butter: {
+              average_rating: 5.0
+            }
           }
-        }
-      ).update
+        ).update
 
-      expect(response).to be_ok
-      expect(response.body[:average_rating]).to eq 5.0
+        expect(response).to be_unauthorized
+      end
     end
 
-    it 'does not allow updating PB if the user is not an admin' do
-      pb = PeanutButter.create!(
-        brand: 'Jif',
-        price_per_ounce: 13.8,
-        average_rating: 3.5,
-        color: 'not green'
-      )
+    context 'when no user session exists' do
+      it 'responds with 401' do
+        pb = PeanutButter.create!(
+          brand: 'Jif',
+          price_per_ounce: 13.8,
+          average_rating: 3.5,
+          color: 'not green'
+        )
 
-      user = User.create!(
-        given_names: 'Weston K.',
-        surnames: 'Dransfield',
-      )
-
-      response = PeanutButterController.new(
-        user: user,
-        params: {
-          id: pb.id,
-          peanut_butter: {
-            average_rating: 5.0
+        response = PeanutButterController.new(
+          user: nil,
+          params: {
+            id: pb.id,
+            peanut_butter: {
+              average_rating: 5.0
+            }
           }
-        }
-      ).update
+        ).update
 
-      expect(response).to be_unauthorized
+        expect(response).to be_unauthorized
+      end
     end
   end
 end
