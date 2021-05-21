dc11d0b90eed843bc2991420324ebcc84a75da12
Weston Dransfield <wdransfield@instructure.com>
2021-05-21 15:11:53 -0600

    5. Use "subject"
    If you have several tests related to the same subject
    use subject{} to DRY them up.
    
    Using a common subject also plays into the strengths of using
    "context". Many contexts that assert on the same subject can
    have top-level "let" items overwritten within teach context.
    
    This makes adding a new example to an existing context fairly
    trivial.

--- spec/controllers/peanut_butter_controller_spec.rb
+++ spec/controllers/peanut_butter_controller_spec.rb
@@ -20,52 +20,65 @@ describe PeanutButterController do
   end
 
   describe '#show' do
+    let(:params) { { id: pb.id } }
+    subject do
+      PeanutButterController.new(
+        user: user, params: params
+      ).show
+    end
+
     context 'when the peanut butter exists' do
       it 'responds with status 200' do
-        response = PeanutButterController.new(
-          user: user, params: { id: pb.id }
-        ).show
-
-        expect(response).to be_ok
+        expect(subject).to be_ok
       end
 
       it 'responds with the requested peanut butter' do
-        response = PeanutButterController.new(
-          user: user, params: { id: pb.id }
-        ).show
-
-        expect(response.body).to include pb.to_h
+        expect(subject.body).to include pb.to_h
       end
     end
 
     context 'when no user session exists' do
-      it 'renders 401' do
-        response = PeanutButterController.new(
-          user: nil, params: { id: pb.id }
-        ).show
+      let(:user) { nil }
 
-        expect(response).to be_unauthorized
+      it 'renders 401' do
+        expect(subject).to be_unauthorized
       end
 
       it 'includes an "unauthorized" message' do
-        response = PeanutButterController.new(
-          user: nil, params: { id: pb.id }
-        ).show
-
-        expect(response.body[:message]).to eq 'Requires an active user session'
+        expect(subject.body[:message]).to eq 'Requires an active user session'
       end
     end
 
     context 'when the peanut butter does not exist' do
+      let(:params) { { id: 222 } }
+
       it 'renders 404' do
-        response = PeanutButterController.new(user: user, params: { id: 999 }).show
-        expect(response).to be_not_found
-        expect(response.body[:message]).to eq 'A record with the given ID does not exist'
+        expect(subject).to be_not_found
+      end
+
+      it 'includes a "not found" message' do
+        expect(subject.body[:message]).to eq 'A record with the given ID does not exist'
       end
     end
   end
 
   describe '#update' do
+    subject do
+      PeanutButterController.new(
+        user: user,
+        params: params
+      ).update
+    end
+
+    let(:params) do
+      {
+        id: pb.id,
+        peanut_butter: {
+          average_rating: 5
+        }
+      }
+    end
+
     context 'when the peanut butter exists' do
       let(:user) do
         User.create!(
@@ -76,64 +89,28 @@ describe PeanutButterController do
       end
 
       it 'updates the peanut butter' do
-        PeanutButterController.new(
-          user: user,
-          params: {
-            id: pb.id,
-            peanut_butter: {
-              average_rating: 5.0
-            }
-          }
-        ).update
-
+        subject
         expect(PeanutButter.find(pb.id).average_rating).to eq 5
       end
 
       it 'responds with the updated peanut butter' do
-        response = PeanutButterController.new(
-          user: user,
-          params: {
-            id: pb.id,
-            peanut_butter: {
-              average_rating: 5.0
-            }
-          }
-        ).update
+        expect(subject).to be_ok
+      end
 
-        expect(response).to be_ok
-        expect(response.body[:average_rating]).to eq 5.0
+      it 'responds with the updated peanut butter' do
+        expect(subject.body[:average_rating]).to eq 5.0
       end
     end
 
     context 'when the current user is not an admin' do
       it 'does not allow updating PB' do
-        response = PeanutButterController.new(
-          user: user,
-          params: {
-            id: pb.id,
-            peanut_butter: {
-              average_rating: 5.0
-            }
-          }
-        ).update
-
-        expect(response).to be_unauthorized
+        expect(subject).to be_unauthorized
       end
     end
 
     context 'when no user session exists' do
       it 'responds with 401' do
-        response = PeanutButterController.new(
-          user: nil,
-          params: {
-            id: pb.id,
-            peanut_butter: {
-              average_rating: 5.0
-            }
-          }
-        ).update
-
-        expect(response).to be_unauthorized
+        expect(subject).to be_unauthorized
       end
     end
   end
